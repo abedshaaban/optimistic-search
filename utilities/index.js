f916b4d00bf62e6161485e985818d1290d0b61c4
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-11-26 11:44:47 +0200

    [clean-up]: removed unused code and moved read file functions to a seperate file

--- utilities/index.js
+++ utilities/index.js
@@ -1,35 +1,3 @@
-import { PDFExtract } from "pdf.js-extract";
-import { readFile } from "node:fs/promises";
-import { getTextExtractor } from "office-text-extractor";
+import { readPDF, readWord } from "./read-file.js";
 
-const pdfExtract = new PDFExtract();
-const wordExtractor = getTextExtractor();
-
-export function readPDF(path) {
-  const options = {};
-  let text = "";
-
-  return new Promise((resolve, reject) => {
-    pdfExtract.extract(path, options, (err, data) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-
-      for (let i = 0; i < data?.pages.length; i++) {
-        const page = data?.pages[i];
-        for (let j = 0; j < page?.content?.length; j++) {
-          const line = page?.content[j];
-          text += line.str;
-        }
-      }
-      resolve(text);
-    });
-  });
-}
-
-export async function readWord(path) {
-  const text = await wordExtractor.extractText({ input: path, type: "file" });
-
-  return text;
-}
+export { readPDF, readWord };
---
+++ utilities/read-file.js
@@ -0,0 +1,34 @@
+import { PDFExtract } from "pdf.js-extract";
+import { getTextExtractor } from "office-text-extractor";
+
+const pdfExtract = new PDFExtract();
+const wordExtractor = getTextExtractor();
+
+export function readPDF(path) {
+  const options = {};
+  let text = "";
+
+  return new Promise((resolve, reject) => {
+    pdfExtract.extract(path, options, (err, data) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      for (let i = 0; i < data?.pages.length; i++) {
+        const page = data?.pages[i];
+        for (let j = 0; j < page?.content?.length; j++) {
+          const line = page?.content[j];
+          text += line.str;
+        }
+      }
+      resolve(text);
+    });
+  });
+}
+
+export async function readWord(path) {
+  const text = await wordExtractor.extractText({ input: path, type: "file" });
+
+  return text;
+}
