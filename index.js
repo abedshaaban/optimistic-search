1cb36ce886181283ef35a28b94ca6c16213b44a9
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-11-26 22:38:49 +0200

    [core]: get data from express

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import { getStore } from "./utilities/index.js";
 import { Engine } from "./core/index.js";
 import express from "express";
+import cors from "cors";
 
 const store = await getStore();
 const searchEngine = new Engine(store.base_folder);
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
 if (store?.storage?.length === 0) {
   await searchEngine.indexFiles();
@@ -12,6 +18,7 @@ if (store?.storage?.length === 0) {
 }
 
 const app = express();
+app.use(cors(corsOptions));
 const port = 5555;
 
 app.get("/search", async (req, res) => {
